bca33f07307665477500b063a4c9e87def67c7ea
Marlus Araujo <m@marlus.com>
2023-03-20 12:53:12 +0700

    Update easing.py

--- easing.py
+++ easing.py
@@ -143,83 +143,63 @@ def ease_in_out_circ(t):
         return 0.5 * (math.sqrt(-4 * (t - 2)*t + 3) + 1)
 
 # Back
-def ease_in_back(t):
-    s = 1.70158
-    return t*t*((s+1)*t - s)
+def ease_in_back(t, overshoot=1.70158):
+    return t*t*((overshoot+1)*t - overshoot)
 
-def ease_out_back(t):
-    s = 1.70158
+def ease_out_back(t, overshoot=1.70158):
     t -= 1
-    return t*t*((s+1)*t + s) + 1
+    return t*t*((overshoot+1)*t + overshoot) + 1
 
-def ease_in_out_back(t):
-    s = 1.70158 * 1.525
-    if t < 0.5:
-        return 2*t*t*((s+1)*2*t - s) / 2
+def ease_in_out_back(t, overshoot=1.70158):
+    overshoot *= 1.525
+    t *= 2
+    if t < 1:
+        return 0.5*(t*t*((overshoot+1)*t - overshoot))
     else:
         t -= 2
-        return (2*t*t*((s+1)*2*t + s) + 2) / 2
+        return 0.5*(t*t*((overshoot+1)*t + overshoot) + 2)
 
 # Elastic
-def ease_in_elastic(t):
-    c = 1
-    p = 0.3
-    if t == 0:
-        return 0
-    elif t == 1:
-        return 1
-    else:
-        a = c
-        s = p / (2*math.pi) * math.asin(1/a)
-        t -= 1
-        return -(a*math.pow(2, 10*t) * math.sin((t-s)*(2*math.pi)/p))
+def ease_in_elastic(t, amplitude=1, period=0.3):
+    return 1 - ease_out_elastic(1 - t, amplitude=amplitude, period=period)
 
-def ease_out_elastic(t):
-    c = 1
-    p = 0.3
-    if t == 0:
-        return 0
-    elif t == 1:
-        return 1
-    else:
-        a = c
-        s = p / (2*math.pi) * math.asin(1/a)
-        return a*math.pow(2, -10*t) * math.sin((t-s)*(2*math.pi)/p) + 1
+def ease_out_elastic(t, amplitude=1, period=0.3):
+    if t == 0 or t == 1:
+        return t
 
-def ease_in_out_elastic(t):
-    c = 1
-    p = 0.3*1.5
-    if t == 0:
-        return 0
-    elif t == 1:
-        return 1
+    s = period / (2 * math.pi) * math.asin(1 / amplitude)
+    return amplitude * math.pow(2, -10 * t) * math.sin((t - s) * (2 * math.pi) / period) + 1
+
+def ease_in_out_elastic(t, amplitude=1, period=0.3):
+    if t == 0 or t == 1:
+        return t
+
+    t *= 2
+    if t < 1:
+        return 0.5 * ease_in_elastic(t, amplitude=amplitude, period=period)
     else:
-        a = c
-        s = p / (2*math.pi) * math.asin(1/a)
-        t *= 2
-        if t < 1:
-            t -= 1
-            return -0.5*(a*math.pow(2,10*t) * math.sin( (t-s)*(2*math.pi)/p ))
-        else:
-            t -= 1
-            return a*math.pow(2,-10*t) * math.sin( (t-s)*(2*math.pi)/p )*0.5 + 1
+        return 0.5 * ease_out_elastic(t - 1, amplitude=amplitude, period=period) + 0.5
+
 
 # Bounce
 def ease_in_bounce(t):
     return 1 - ease_out_bounce(1 - t)
 
 def ease_out_bounce(t):
-    if t < 4/11.0:
-        return (121 * t * t) / 16.0
-    elif t < 8/11.0:
-        return (363/40.0 * t * t) - (99/10.0 * t) + 17/5.0
-    elif t < 9/10.0:
-        return (4356/361.0 * t * t) - (35442/1805.0 * t) + 16061/1805.0
+    if t < 1 / 2.75:
+        return 7.5625 * t ** 2
+    elif t < 2 / 2.75:
+        t -= 1.5 / 2.75
+        return 7.5625 * t ** 2 + 0.75
+    elif t < 2.5 / 2.75:
+        t -= 2.25 / 2.75
+        return 7.5625 * t ** 2 + 0.9375
     else:
-        return (54/5.0 * t * t) - (513/25.0 * t) + 268/25.0
+        t -= 2.625 / 2.75
+        return 7.5625 * t ** 2 + 0.984375
 
 def ease_in_out_bounce(t):
     if t < 0.5:
-        return 0.5 * ease_in_bounce(t*2)
+        return 0.5 * ease_in_bounce(t * 2)
     else:
-        return 0.5 * ease_out_bounce(t*2 - 1) + 0.5
+        return 0.5 * ease_out_bounce(t * 2 - 1) + 0.5
